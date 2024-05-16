629783786d9a61e2ef0173288eb49a72e7f6046e
gsuarez90 <60275264+gsuarez90@users.noreply.github.com>
2024-05-15 22:07:39 -0700

    Update javascrill.js
    Editing javaScrill code to make motion in from the right side

--- javascrill.js
+++ javascrill.js
@@ -1,4 +1,6 @@
 window.onload = function() {
   const nameElement = document.getElementById("name");
+  nameElement.style.position = "relative";
+  nameElement.style.right = "100%";
   nameElement.classList.add("glide");
 };
